22ac481384aef2f8766ff0c692f820dff1542eac
Dustin Hayes <notnot.dustinhayes+github@gmail.com>
2026-07-26 22:32:08 -0400

    fix adapter issue

--- apps/whats-on/astro.config.mjs
+++ apps/whats-on/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import decapCmsOauth from 'astro-decap-cms-oauth';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [decapCmsOauth()]
 });
